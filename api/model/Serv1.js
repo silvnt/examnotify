210c710a155e854551c1adc56d8426027bd67d9e
wedsonxse <wedsonlino27@gmail.com>
2018-04-28 22:40:05 -0300

    mudança no formato da mensagem udp a ser enviada

--- api/model/Serv1.js
+++ api/model/Serv1.js
@@ -2,7 +2,7 @@ var PORT = 1234; //porta
 var HOST = '127.0.0.1'; //ip
 
 const dgram = require('dgram');
-const registro = Buffer.from({name:'consultarExame', type: 'set'});
+const registro = Buffer.from(['consultarExame','set']);
 const serv1 = dgram.createSocket('udp4');
 serv1.send(registro, 1234, 'HOST', (err) => {
  serv1.close();
